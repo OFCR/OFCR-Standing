c0ec2191b46cce248da61afe31493e0e9b64fc75
OMGCA <t.seb.xia@gmail.com>
2018-12-28 19:05:50 +0000

    Testing VSCode Git

--- src/xiatstudio/XMLHandler.java
+++ src/xiatstudio/XMLHandler.java
@@ -1,5 +1,21 @@
 package xiatstudio;
 
-public class XMLHandler {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class XMLHandler extends DefaultHandler {
+	List<Driver> driverList = null;
+	
+	public List<Driver> getDriverList(){
+		return driverList;
+	}
+
+	@Override
+	public void startElement(String uri, String localElement, String qName, Attributes attributes) throws SAXException{
+
+	}
 }
